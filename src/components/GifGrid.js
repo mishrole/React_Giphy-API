c634bc51e27729ed3b32aaebef593efdfff6405f
Mitchell Rodríguez <mitrodle@gmail.com>
2021-03-29 12:32:50 -0500

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 require('dotenv').config();
 
 export const GifGrid = ({ category }) => {
+
+    const [count, setCount] = useState(0);
+
+    /*  UseEffect ejecutar algo de manera condicional
+        Si lo pasamos sin dependencias [], sólo se ejecutará una vez
+        De esta forma, sólo se ejecuta getGifs cuando el componente
+        Es renderizado por primera vez 
+    */
+   
+    useEffect(() => {
+        getGifs();
+    }, []);
 
     const getGifs = async () => {
         const endpoint = 'http://api.giphy.com/v1/gifs/search';
@@ -24,11 +36,15 @@ export const GifGrid = ({ category }) => {
         console.log(gifs)
     }
 
-    getGifs();
+    // getGifs();
 
     return (
         <div>
             <h3>{category}</h3>
+            <h4> {count}</h4>
+            {/* Cada click vuelve a disparar getGifs
+            Solución: useEffect */}
+            <button onClick={() => setCount(count + 1)}></button>
         </div>
     )
 }
